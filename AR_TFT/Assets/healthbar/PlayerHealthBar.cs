fb830ce99ecb23fa5e11b7412e60ff8e8d40ccac
Mython <swag0090@naver.com>
2021-12-04 23:05:06 +0900

    player hp

---
+++ AR_TFT/Assets/healthbar/PlayerHealthBar.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PlayerHealthBar : MonoBehaviour
+{
+    public GameObject PlayerHP;
+    public int hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+            PlayerHP.GetComponent<RectTransform>().sizeDelta = new Vector2(hp, 100);
+    }
+}
